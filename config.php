dfa862b3b55377264e8db17d7882633f9500248e
Vashishth Patel <76911582+vasu-1@users.noreply.github.com>
2021-09-15 08:30:47 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
 
-            $servername = "mysql-46557-0.cloudclusters.net";
-            $username = "admin";
-            $password = "jmfYEp69";
-            $database = "test";
+//             $servername = "mysql-46557-0.cloudclusters.net";
+//             $username = "admin";
+//             $password = "jmfYEp69";
+//             $database = "test";
             
-            // $servername = "localhost";
-            // $username = "root";
-            // $password = "";
-            // $database = "test";
-            // $conn = mysqli_connect( $servername , $username , $password , $database );
+            $servername = "localhost";
+            $username = "root";
+            $password11 = "";
+            $database = "test";
+            $conn = mysqli_connect( $servername , $username , $password11 , $database );
 
         ?>
